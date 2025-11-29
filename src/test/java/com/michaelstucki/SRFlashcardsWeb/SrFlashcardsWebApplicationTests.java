ffe35d16843468214cfa9a8fb76bddfa2cd4a379
michaelstucki <stcuthbertus@protonmail.com>
2025-11-29 09:47:12 -0700

    initial commit

---
+++ src/test/java/com/michaelstucki/SRFlashcardsWeb/SrFlashcardsWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michaelstucki.SRFlashcardsWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrFlashcardsWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
